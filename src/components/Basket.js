e73848fc6112e35f9b475d675089e21bdbd72136
Ju1ija <j.lipnickaja@gmail.com>
2021-11-27 20:17:40 +0000

    added an image for empty basket

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,4 +1,5 @@
 import "./Basket.css";
+import continueShopping from "../continueShopping.jpeg";
 
 const Basket = (props) => {
   const removeCartItem = (index, item) => {
@@ -11,19 +12,32 @@ const Basket = (props) => {
 
   return (
     <div className="cart">
-      <h2>My Cart</h2>
-      {props.basket.map((item, index) => {
-        return (
-          <div className="shopping-cart">
-            <img src={item.url} alt="" />
-            <h4>{item.name}</h4>
-            <p>£{item.price}</p>
-            <button onClick={() => removeCartItem(index, item)}>delete</button>
-          </div>
-        )
-      })}
+      {props.basket.length === 0 ? <BasketEmpty /> : (
+        <>
+          <h2>My Cart</h2>
+          {props.basket.map((item, index) => {
+            return (
+              <div className="shopping-cart">
+                <img src={item.url} alt="" />
+                <h4>{item.name}</h4>
+                <p>£{item.price}</p>
+                <button onClick={() => removeCartItem(index, item)}>delete</button>
+              </div>
+            )
+          })}
+        </>
+      )}
       <h4>Total to pay: £{(props.totalCost).toFixed(2)}</h4>
     </div>
+  )
+}
+
+const BasketEmpty = () => {
+  return (
+    <>
+      <h4>Uh oh! Your basket is empty, lets find a furry friend to add!</h4>
+      <img src={continueShopping} alt="" />
+    </>
   )
 }
 
